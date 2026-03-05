4319fa9643ce396b0b1f661ef9b271340a49f14a
antares <antares@alyr.dev>
2026-03-05 21:58:42 +0800

    Add sse4-improve-ctz-concatenate to test

--- compare.sh
+++ compare.sh
@@ -2,4 +2,5 @@
 ./tools/compile_cpp_sse4.sh ./zmij-cpp/zmij.cc zmijcpp
 ./tools/compile_cpp_sse4.sh ./sse4-improve/zmij.cc sse4_even_faster
 ./tools/compile_cpp_sse4.sh ./sse4-improve-ctz/zmij.cc sse4_even_faster_ctz
-./build/any_dtoa_benchmark ./build/libs/libzmijcpp.so:zmijcpp_detail_write_double:skip ./build/libs/libsse4_even_faster.so:zmijcpp_detail_write_double:skip ./build/libs/libsse4_even_faster_ctz.so:zmijcpp_detail_write_double:skip
+./tools/compile_cpp_sse4.sh ./sse4-improve-ctz-concatenate/zmij.cc sse4_even_faster_ctz-concatenate
+./build/any_dtoa_benchmark ./build/libs/libzmijcpp.so:zmijcpp_detail_write_double:skip ./build/libs/libsse4_even_faster.so:zmijcpp_detail_write_double:skip ./build/libs/libsse4_even_faster_ctz.so:zmijcpp_detail_write_double:skip ./build/libs/libsse4_even_faster_ctz-concatenate.so:zmijcpp_detail_write_double:skip
